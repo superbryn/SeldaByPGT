f7740be6ef433f0c7a200d385e41a2ca418b5ccc
xenogen <151805539+superbryn@users.noreply.github.com>
2024-10-16 19:58:16 +0530

    Update Textcard.jsx

--- src/cards/Textcard.jsx
+++ src/cards/Textcard.jsx
@@ -4,7 +4,7 @@ import './Textcard.css'
 function Textcard() {
   return (
     <div className="textcard">
-        <p>Team UnderDogs is a dedicated group of seven members, <br />
+        <p>Team SELDA is a dedicated group of seven members, <br />
         working together to advance AI technology for sign language interpretation. Under the leadership of Anna Rose, <br />
         a democratic and inclusive leader, the team emphasizes collaboration and shared decision-making to enhance accessibility <br />
         for the hearing impaired. With a focus on translating hand signs into text or speech, Team UnderDogs aims to break communication <br />
